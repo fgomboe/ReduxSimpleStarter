c5af062a818ca8675ead42d559ee6c81c3e1a4ac
Andres Gomez <andresgomez@seven4n.com>
2017-03-27 19:09:07 -0500

    Por el video 29

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,3 +1,22 @@
 /**
  * Created by fgomboe on 26/03/17.
  */
+
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = ({videos, onVideoSelect}) => {
+
+    return (
+        <ul className="col-md-4 list-group">
+            { videos.map(v =>
+                <VideoListItem
+                    key={ v.etag }
+                    video={ v }
+                    onVideoSelect={onVideoSelect}/>
+            ) }
+        </ul>
+    );
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,46 @@
  * Created by fgomboe on 23/03/17.
  */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAU1UEq_cp_JaGmXn7xfw6sMyFmYpRHa-0';
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({key: API_KEY, term: 'pink floyd'}, (videos) => {
+            console.log(videos);
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[1]
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={ this.state.selectedVideo }/>
+                <VideoList
+                    videos={ this.state.videos }
+                    onVideoSelect= {selectedVideo => this.setState({selectedVideo})} />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
